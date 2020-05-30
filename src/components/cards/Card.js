fad8651f5e5776d0bd33ab362279b73abd29a5b7
maseug <mas.eug@yandex.ru>
2020-05-30 19:23:25 +0300

    styles tag

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -13,7 +13,7 @@ function Card(props) {
         }}
        className="card" onClick={() => props.openCard(props.country.name)}>
         <div className="card__box">
-            <div className="image" style={{backgroundImage: "url(" + props.country.flag + ")"}} alt="flag" />
+            <div className="image" style={{backgroundImage: "url(" + props.country.flag + ")"}} />
             <div className="desc">
                 <div className="name">{props.country.name}</div>
                 <div className="title population"><span className="normal">Population: </span> {props.country.population}</div>
